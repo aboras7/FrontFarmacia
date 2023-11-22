63f4d01fa38665c4328dc75bc8229370d87f5bc1
Eduardo Nogueira <eduardo7_@hotmail.com>
2023-11-22 12:06:37 -0300

    correção cores

--- src/components/produtos/cardProdutos/CardProdutos.tsx
+++ src/components/produtos/cardProdutos/CardProdutos.tsx
@@ -6,7 +6,7 @@ function CardProdutos() {
             flex flex-col rounded overflow-hidden justify-between'>
                 
             <div>
-                <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+                <div className="flex w-full bg-purple-700 py-2 px-4 items-center gap-4">
                     <img src='' className='h-12 rounded-full' alt="" />
                     <h3 className='text-lg font-bold text-center uppercase'>Nome:</h3>
                 </div>
@@ -18,11 +18,11 @@ function CardProdutos() {
                 </div>
             </div>
             <div className="flex">
-                <Link to='' className='w-full text-white bg-indigo-400 
+                <Link to='' className='w-full text-white bg-purple-700 
                     hover:bg-indigo-800 flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
-                <Link to='' className='text-white bg-red-400 
+                <Link to='' className='text-white bg-red-500 
                     hover:bg-red-700 w-full flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
